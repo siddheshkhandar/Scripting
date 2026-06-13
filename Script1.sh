dba6e291567d608bc80a3feb9d747531d2528600
Siddhesh Khandar <143962064+siddheshkhandar@users.noreply.github.com>
2026-06-13 13:15:16 +0530

    Add additional information to Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,2 +1,3 @@
 echo "my name is siddhesh khandar and i am learning linux scripting"
 echo "i am from batch 32 ygminds"
+echo "Currently i am workinf as an devops engineer with 3 years of an experience"
